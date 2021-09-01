dd5323b3e68d5939fda33ec12b31c820cf8e5028
Pirulax <patrikjankovics7@gmail.com>
2021-09-01 02:50:46 +0200

    Render

--- source/game_sa/CRope.cpp
+++ source/game_sa/CRope.cpp
@@ -7,7 +7,7 @@ void CRope::InjectHooks() {
     // Install("CRope", "CreateHookObjectForRope", 0x556070, &CRope::CreateHookObjectForRope);
     // Install("CRope", "UpdateWeightInRope", 0x5561B0, &CRope::UpdateWeightInRope);
     // Install("CRope", "Remove", 0x556780, &CRope::Remove);
-    // Install("CRope", "Render", 0x556800, &CRope::Render);
+    Install("CRope", "Render", 0x556800, &CRope::Render);
     // Install("CRope", "PickUpObject", 0x5569C0, &CRope::PickUpObject);
     // Install("CRope", "Update", 0x557530, &CRope::Update);
 }
@@ -40,7 +40,62 @@ void CRope::Remove() {
 
 // 0x556800
 void CRope::Render() {
-    plugin::CallMethod<0x556800, CRope*>(this);
+    // Note: Probably needs adjustments if `NUM_ROPE_SEGMENTS` is changed
+    if (!TheCamera.IsSphereVisible(m_segments[NUM_ROPE_SEGMENTS / 2], 20.0f))
+        return;
+
+    if ((TheCamera.GetPosition() - m_segments[0]).Magnitude2D() >= 120.0f)
+        return;
+
+    DefinedState();
+
+    const RwRGBA color = { 128, 0, 0, 0 };
+
+    const auto GetVertex = [](unsigned i) {
+        return &aTempBufferVertices[i];
+    };
+
+    RwRenderStateSet(rwRENDERSTATEZWRITEENABLE, (void*)TRUE);
+    RwRenderStateSet(rwRENDERSTATEVERTEXALPHAENABLE, (void*)TRUE);
+    RwRenderStateSet(rwRENDERSTATESRCBLEND, (void*)rwBLENDSRCALPHA);
+    RwRenderStateSet(rwRENDERSTATEDESTBLEND, (void*)rwBLENDINVSRCALPHA);
+    RwRenderStateSet(rwRENDERSTATETEXTUREFILTER, (void*)rwFILTERLINEAR);
+    RwRenderStateSet(rwRENDERSTATETEXTURERASTER, (void*)FALSE);
+
+    for (unsigned i = 0; i < NUM_ROPE_SEGMENTS; i++) {
+        RxObjSpace3DVertexSetPreLitColor(GetVertex(i), &color);
+        RxObjSpace3DVertexSetPos(GetVertex(i), &m_segments[i]);
+    }
+
+
+    if (RwIm3DTransform(aTempBufferVertices, NUM_ROPE_SEGMENTS, 0, 0))
+    {
+        RxVertexIndex indices[] = {
+            0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6,
+            6, 7, 7, 8, 8, 9, 9, 10, 10, 11, 11,
+            12, 12, 13, 13, 14, 14, 15, 15, 16,
+            16, 17, 17, 18, 18, 19, 19, 20, 20,
+            21, 21, 22, 22, 23, 23, 24, 24, 25,
+            25, 26, 26, 27, 27, 28, 28, 29, 29,
+            30, 30, 31
+        };
+        RwIm3DRenderIndexedPrimitive(rwPRIMTYPELINELIST, indices, std::size(indices));
+        RwIm3DEnd();
+    }
+
+    if (m_type == 6) {
+        const CVector pos[] = { m_segments[0], {709.32f, 916.20f, 53.0f} };
+        for (unsigned i = 0; i < 2; i++) {
+            RxObjSpace3DVertexSetPreLitColor(GetVertex(i), &color);
+            RxObjSpace3DVertexSetPos(GetVertex(i), &pos[i]);
+        }
+        if (RwIm3DTransform(aTempBufferVertices, 2, 0, 0))
+        {
+            RxVertexIndex indices[] = { 0, 1 };
+            RwIm3DRenderIndexedPrimitive(rwPRIMTYPELINELIST, indices, 2);
+            RwIm3DEnd();
+        }
+    }
 }
 
 // 0x5569C0
