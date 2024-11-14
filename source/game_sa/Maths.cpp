1c296fa3cfa6de1c35cfbbf6a8a124e0c4a98bb7
Kamil Marciniak <forkerer1@gmail.com>
2024-11-14 21:21:16 +0100

    Fix few water related issues (#805)
    * fix CalculateWavesOnlyForCoordinate and SplitWaterRectangleAlong(X/Y)Line
    * Fix BlockHit
    * Fix AddWaterLevelQuad and AddWaterLevelTriangle - replace quad sorting alg for a simpler one, replace tri sorting algoritm with original logic - doesn't seem to follow any proper rules.
    * Move sin/cos generation using precomputed table to CMaths

--- source/game_sa/Maths.cpp
+++ source/game_sa/Maths.cpp
@@ -10,6 +10,8 @@ void CMaths::InjectHooks()
     RH_ScopedCategoryGlobal();
 
     RH_ScopedInstall(InitMathsTables, 0x59AC90);
+    RH_ScopedInstall(GetSinFast, 0x4A1340); // No callers in the game, inlined in every single instance
+    RH_ScopedInstall(GetCosFast, 0x4A1360); // No callers in the game, inlined in every single instance
 }
 
 void CMaths::InitMathsTables() {
@@ -17,4 +19,13 @@ void CMaths::InitMathsTables() {
 
     for (int32 i = 0; i < 256; ++i)
         ms_SinTable[i] = sin(static_cast<float>(i) * PI / 128.0F);
+}
+
+float CMaths::GetSinFast(float rad) {
+    return ms_SinTable[static_cast<uint8>(rad * RadToSinTableIndex) + 1];
+}
+
+float CMaths::GetCosFast(float rad) {
+    // Table has 256 elements, and spans [0:2PI), 64 index move equals PI/2 move on the X axis, and cos(x) == sin(x + PI/2)
+    return ms_SinTable[static_cast<uint8>(rad * RadToSinTableIndex + 64.f) + 1];
 }
--- source/game_sa/Maths.h
+++ source/game_sa/Maths.h
@@ -1,10 +1,18 @@
 #pragma once
 
+constexpr float RadToSinTableIndex = 256.0f / TWO_PI;
+
 class CMaths {
 public:
     static float (&ms_SinTable)[256];
 
     static void InjectHooks();
-
     static void InitMathsTables();
+
+    // Originally named `SinTabel` with argument named `Arg` which sucks, so replaced with custom name.
+    static float GetSinFast(float rad);
+
+    // Originally named `CosTabel` with argument named `Arg`, same story as above
+    static float GetCosFast(float rad);
+
 };
