852e1be9e9e285f7346d9771d41cd368597a8b0d
Pirulax <patrikjankovics7@gmail.com>
2024-11-30 21:36:22 +0100

    Add some generic bugs and a description

--- source/reversiblebugfixes/Bugs.hpp
+++ source/reversiblebugfixes/Bugs.hpp
@@ -2,7 +2,33 @@
 
 #include "./ReversibleBugFix.hpp"
 
+/*!
+* This is where the bugs are defined.
+* The usual naming convention is `ClassName_Function_BugName` (`BugName` is something unique you come up with).
+* Unless the same bug is spanning across multiple functions.
+* 
+* Fixes that prevent OOB should use `GenericOOB`.
+* Fixes that prevent crashes (Like additional null ptr checks) can use `GenericCrash`.
+* Feel free to add a new generic bug type if there's nothing that fits your needs.
+*/
 namespace notsa::bugfixes {
+//
+// Generic
+//
+inline const ReversibleBugFix GenericCrashing{
+    "Generic crashes (null ptr access, etc)",
+    "Fixes bugs that cause null ptr access, and similar anomalies crashing the game",
+    "Contributors"
+};
+inline const ReversibleBugFix GenericOOB{
+    "Generic out-of-bounds bugs",
+    "Fixes generic out-of-bounds bugs across the codebase",
+    "Contributors"
+};
+
+//
+// Other bugs
+//
 inline const ReversibleBugFix PS2CoronaRendering{
     "PS2 Corona Rendering",
     "Fix corona rendering, so they're like on PS2",
