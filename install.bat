b547fa59a275df3747320148a86d5c40f5d8ca2a
yukani <yukani@zohomail.eu>
2025-03-25 00:50:57 +0300

    Add unity build option (#968)
    Unity builds are now the default.
    These builds are much faster than ordinary builds, at (seemingly) no extra cost.

--- install.bat
+++ install.bat
@@ -1,9 +1,12 @@
 @echo off
 
-REM %1: CMake build type [Debug, Release, RelWithDebInfo]
+REM %1: CMake build type [Debug (default), Release, RelWithDebInfo]
+REM %2: CMake profile [conan-default-unity (default), conan-default]
 
 set BUILDTYPE=%1
 if "%BUILDTYPE%"=="" set BUILDTYPE=Debug
-conan install . --build missing -s build_type=%BUILDTYPE% --profile conanprofile.txt
-cmake --preset conan-default
+set PROFILETYPE=%2
+if "%PROFILETYPE%"=="" set PROFILETYPE=conan-default-unity
+conan install . --build missing -s build_type="%BUILDTYPE%" --profile conanprofile.txt
+cmake --preset "%PROFILETYPE%"
 PAUSE
--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# $1: CMake build type [Debug, Release, RelWithDebInfo]
+# $1: CMake build type [Debug (default), Release, RelWithDebInfo]
+# $2: CMake profile [conan-default-unity (default), conan-default]
 
 conan install . --build missing -s build_type=${1:-Debug} --profile conanprofile.txt;
-cmake --preset conan-default;
+cmake --preset ${2:-conan-default-unity};
