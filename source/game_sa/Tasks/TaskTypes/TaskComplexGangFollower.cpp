b51f08dcb380db7093e86a6fa4ac2b3ebf52bad9
Pirulax <patrikjankovics7@gmail.com>
2025-03-24 23:21:22 +0100

    `CTaskComplexAvoidPedWhileWandering` (#556)

--- source/game_sa/Tasks/TaskTypes/TaskComplexGangFollower.cpp
+++ source/game_sa/Tasks/TaskTypes/TaskComplexGangFollower.cpp
@@ -8,7 +8,7 @@ void CTaskComplexGangFollower::InjectHooks() {
 
     RH_ScopedInstall(Constructor, 0x65EAA0, { .reversed = false });
     RH_ScopedInstall(Destructor, 0x65EBB0, { .reversed = false });
-    RH_ScopedInstall(CalculateOffsetPosition, 0x65ED40, { .reversed = false });
+    //RH_ScopedInstall(CalculateOffsetPosition, 0x65ED40, { .reversed = false }); // not hooked because i want to keep CVector return, but original function took a CVector&
     RH_ScopedInstall(Clone, 0x65ECB0, { .reversed = false });
     RH_ScopedInstall(MakeAbortable, 0x65EC30, { .reversed = false });
     RH_ScopedInstall(CreateNextSubTask, 0x665E00, { .reversed = false });
@@ -28,8 +28,10 @@ CTaskComplexGangFollower::~CTaskComplexGangFollower() {
 }
 
 // 0x65ED40
-void CTaskComplexGangFollower::CalculateOffsetPosition(CVector& pos) {
-    plugin::CallMethod<0x65ED40, CTaskComplexGangFollower*, CVector&>(this, pos);
+CVector CTaskComplexGangFollower::CalculateOffsetPosition() {
+    CVector ret;
+    plugin::CallMethod<0x65ED40, CTaskComplexGangFollower*>(this, &ret);
+    return ret;
 }
 
 // 0x65ECB0
