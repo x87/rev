a377336c5e3f483aef9bdbd9b13aac42e498524a
Pirulax <patrikjankovics7@gmail.com>
2025-05-02 01:06:02 +0200

    `PlacePedAtEffect` (#1066)

--- source/game_sa/Attractors/PedAttractorPedPlacer.cpp
+++ source/game_sa/Attractors/PedAttractorPedPlacer.cpp
@@ -1,6 +1,7 @@
 #include <StdInc.h>
 #include <Plugins/TwoDEffectPlugin/2dEffect.h>
 
+#include "PedPlacement.h"
 #include "PedAttractorPedPlacer.h"
 
 
@@ -8,10 +9,24 @@ void CPedAttractorPedPlacer::InjectHooks() {
     RH_ScopedClass(CPedAttractorPedPlacer);
     RH_ScopedCategory("Attractors");
 
-    RH_ScopedGlobalInstall(PlacePedAtEffect, 0x5EA390, {.reversed = false});
+    RH_ScopedGlobalInstall(PlacePedAtEffect, 0x5EA390);
 }
 
 // 0x5EA390
-void CPedAttractorPedPlacer::PlacePedAtEffect(C2dEffectPedAttractor const& effect, CEntity* entity, CPed* ped, float forwardOffsetMultiplier) {
-    return plugin::CallAndReturn<void, 0x5EA390, C2dEffectPedAttractor const&, CEntity*, CPed*, float>(effect, entity, ped, forwardOffsetMultiplier);
+void CPedAttractorPedPlacer::PlacePedAtEffect(C2dEffectPedAttractor const& fx, CEntity* entity, CPed* ped, float forwardOffsetMultiplier) {
+    const auto mat = entity
+        ? entity->GetMatrix()
+        : CMatrix::Unity();
+
+    const auto fxPos      = CPedAttractorManager::ComputeEffectPos(&fx, mat);
+    const auto fxUseDir   = CPedAttractorManager::ComputeEffectUseDir(&fx, mat);
+    const auto fxFwdDir   = CPedAttractorManager::ComputeEffectForwardDir(&fx, mat);
+    //const auto fxQueueDir = CPedAttractorManager::ComputeEffectQueueDir(&fx, mat); // NOTSA: Actually unused
+
+    ped->SetPosn(std::get<CVector>(CPedPlacement::FindZCoorForPed(fxPos + fxFwdDir * forwardOffsetMultiplier)));
+
+    const auto angle = CVector2D{ fxUseDir }.Heading();
+    ped->GetMatrix().SetRotateZOnly(angle);
+    ped->m_fAimingRotation = angle;
+    ped->m_fCurrentRotation = angle;
 }
